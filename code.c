43e926783d29fd1a7380764e5cabe81a99ab5f63
Varun-thecoderman <90124421+Varun-thecoderman@users.noreply.github.com>
2021-10-27 22:34:50 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,15 @@
+#include <reg51.h>
+void main(void)
+{
+unsigned char mybyte;
+unsigned int i,j;
+P1=0xFF;                
+while (1)
+ {
+  mybyte=P1;                
+  for(i=0;i<=2555;i++)
+   for(j=0;j<=255;j++)
+   {   }
+  P2=mybyte;                
+ }
+}
